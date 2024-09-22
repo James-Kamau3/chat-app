4304eeacd50bc62a1f7d9c9a0a9cbf264836d226
James <kamaujames3705@gmail.com>
2024-09-22 15:11:43 +0300

    included app to settings

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'daphne',
     #add Django Channels
     'channels',
+    'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
